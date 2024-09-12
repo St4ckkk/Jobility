2fe3156ae5a94774f60e12442e7642afc8f918b7
St4ckkk <keyanandydelgado@gmail.com>
2024-09-12 18:19:20 +0800

    hotdog

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/sign-in',  
+      initialRoute: '/splash',  
       onGenerateRoute: AppRouter.generateRoute,  
     );
   }
