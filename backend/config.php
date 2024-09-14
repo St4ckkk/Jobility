4b83716bec181358e6641d7e7ec72461fcdc8295
St4ckkk <keyanandydelgado@gmail.com>
2024-09-14 13:08:43 +0800

    backend structure

---
+++ backend/config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'database' => [
+        'host' => 'localhost',
+        'dbname' => 'jobility',
+        'username' => 'root',
+        'charset' => 'utf8mb4'
+    ],
+    
+];
