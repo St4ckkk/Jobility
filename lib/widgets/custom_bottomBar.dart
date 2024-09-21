85ac5791005aa587f28f9915216b71ed7c21e68e
St4ckkk <keyanandydelgado@gmail.com>
2024-09-22 07:32:21 +0800

    good morning bitches

--- lib/widgets/custom_bottomBar.dart
+++ lib/widgets/custom_bottomBar.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:jobility_app/core/config/imports.dart';
 
 class CustomBottomBar extends StatefulWidget {
   final int currentIndex;
   final Function(int) onTap;
+  final double height;
 
-  const CustomBottomBar({
-    Key? key,
-    required this.currentIndex,
-    required this.onTap,
-  }) : super(key: key);
+  const CustomBottomBar(
+      {Key? key,
+      required this.currentIndex,
+      required this.onTap,
+      this.height = 70.0})
+      : super(key: key);
 
   @override
   State<CustomBottomBar> createState() => _CustomBottomBarState();
@@ -17,35 +20,61 @@ class CustomBottomBar extends StatefulWidget {
 class _CustomBottomBarState extends State<CustomBottomBar> {
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed, // All items are visible
-      currentIndex: widget.currentIndex,
-      onTap: widget.onTap,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
+    return Container(
+      height: widget.height,
+      decoration: BoxDecoration(
+        color: AppColors.lightColor,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30.0),
+          topRight: Radius.circular(30.0),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.group),
-          label: 'My Network',
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: const Offset(0, -3), // Shadow position
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.add_box),
-          label: 'Post',
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: widget.currentIndex,
+          onTap: widget.onTap,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.group),
+              label: 'My Network',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_box),
+              label: 'Post',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: 'Notifications',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.work),
+              label: 'Jobs',
+            ),
+          ],
+          selectedItemColor: AppColors.primaryColor,
+          unselectedItemColor: AppColors.grayAccentColor,
+          showUnselectedLabels: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.notifications),
-          label: 'Notifications',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.work),
-          label: 'Jobs',
-        ),
-      ],
-      selectedItemColor: Colors.blue, // Primary color
-      unselectedItemColor: Colors.grey,
-      showUnselectedLabels: true,
+      ),
     );
   }
 }
